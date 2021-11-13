0c210b41f5499b75d06db75a67cfdefbd7567c0e
S-tanishi <tanichi.u.n.20@gmail.com>
2021-11-14 08:12:01 +0900

    q2: ２つの配列を交互の要素を取り結合せよ

---
+++ algorithm/questions/question2_merge.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+問題2
+交互に要素を取ることで、2つのリストを結合する関数を記述せよ。
+例えば [a, b, c]と[1, 2, 3]という2つのリストを与えると、関数は [a, 1, b, 2, c, 3]を返す。
+ */
+
+
+$list1 = array('a','b','c','d','e','f');
+$list2 = array(1,2,3,4,5,6,7,8,9,10);
+
+echo '<pre>';
+print_r($list1);
+print_r($list2);
+print_r(getAlternationArrayMerge($list1, $list2));
+
+// whileをつかい要素のwaveごとに一つずつカットし格納
+function getAlternationArrayMerge($list1, $list2)
+{
+    $answer = [];
+
+    while (true)
+    {
+        if (empty($list1) && empty($list2)) {
+            break;
+        }
+        if ($list1) {
+            $answer[] = array_shift($list1);
+        }
+        if ($list2) {
+            $answer[] = array_shift($list2);
+        }
+    }
+
+    return $answer;
+}
